b8228ef86b73435b12773336fdbf78836435b982
dhimasadip <dhimasadip.dap@gmail.com>
2020-08-21 00:52:18 +0700

    add readme

--- client/app/config/graphql.js
+++ client/app/config/graphql.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 
 const client = new ApolloClient({
-    uri: 'http://192.168.0.14:3001/',
+    uri: 'http://192.168.0.14:3001/', // Please input IP ADDRESS instead of localhost
     cache: new InMemoryCache()
 })
 
